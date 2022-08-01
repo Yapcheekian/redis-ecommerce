b19234b051745083ace6c96bfefdc207cfd029be
Gerard Yap <cheekian.yap@aiesec.net>
2022-08-01 23:02:21 +0800

    use lua scripting

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,13 +1,5 @@
 import { client } from '$services/redis';
-import { itemsKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
 
 export const incrementView = async (itemId: string, userId: string) => {
-	const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
-
-	if (inserted) {
-		return Promise.all([
-			client.hIncrBy(itemsKey(itemId), 'views', 1),
-			client.zIncrBy(itemsByViewsKey(), 1, itemId)
-		]);
-	}
+	return client.incrementView(itemId, userId);
 };
--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,11 +1,41 @@
-import { createClient } from 'redis';
+import { createClient, defineScript } from 'redis';
+import { itemsKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
 
 const client = createClient({
 	socket: {
 		host: process.env.REDIS_HOST,
 		port: parseInt(process.env.REDIS_PORT)
 	},
-	password: process.env.REDIS_PW
+	password: process.env.REDIS_PW,
+	scripts: {
+		incrementView: defineScript({
+			NUMBER_OF_KEYS: 3,
+			SCRIPT: `
+				local itemViewsKey = KEYS[1]
+				local itemsKey = KEYS[2]
+				local itemsByViewsKey = KEYS[3]
+				local itemId = ARGV[1]
+				local userId = ARGV[2]
+
+				local inserted = redis.call('PFADD', itemViewsKey, userId)
+
+				if inserted == 1 then
+					redis.call('HINCRBY', itemsKey, 'views', 1)
+					redis.call('ZINCRBY', itemsByViewsKey, 1, itemId)
+				end
+			`,
+			transformArguments(itemId: string, userId: string) {
+				return [
+					itemsViewsKey(itemId),
+					itemsKey(itemId),
+					itemsByViewsKey(),
+					itemId,
+					userId
+				];
+			},
+			transformReply() {}
+		})
+	},
 });
 
 client.on('error', (err) => console.error(err));
